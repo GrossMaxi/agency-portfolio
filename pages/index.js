00e779acdfdd04988ef559c1d2697a1c1283a721
GrossMaxi <maximilianmatthias.gross@gmail.com>
2025-12-30 20:30:00 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Our Agency</h1>
+      <p>This is the home page of our agency portfolio site. We deliver creative solutions across design, development, and marketing.</p>
+      <section>
+        <h2>Featured Projects</h2>
+        <ul>
+          <li>Case Study 1</li>
+          <li>Case Study 2</li>
+          <li>Case Study 3</li>
+        </ul>
+      </section>
+    </main>
+  );
+}
